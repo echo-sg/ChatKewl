3defdfd7712137aee79e8298af8f1a582f0a503f
Nafees Nehar <nafees87n@gmail.com>
2020-08-23 16:52:41 +0530

    index.js reverted

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Two from './Components/page-2/two.js';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <React.StrictMode>
     
-    {/* <App /> */}
-    <Two />
+    <App />
   
   </React.StrictMode>,
   document.getElementById('root')
