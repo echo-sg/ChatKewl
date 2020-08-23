5bafceab865d17d78517bb7b4a5fcccfe7543c6b
Nafees Nehar <nafees87n@gmail.com>
2020-08-23 16:50:24 +0530

    logout fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Homepage from "./Components/homepage/Homepage";
 import Dashboard from "./Components/dashboard";
+import Chatroom from "./Components/chats/chatroom1";
 import Three from "./Components/page-3/three";
 import Two from "./Components/two";
 import ProtectedRoute from "./Components/ProtectedRoute";
@@ -21,6 +22,10 @@ class App extends Component {
           <Route exact={true} path="/two" component={Two} />
           <Route exact={true} path="/login/redirect" component={Loading} />
           <Route exact={true} path="/three" component={Three} />
+          <Route exact path="/room/roomId" component="Chatroom ">
+            <h2> HEY !!! </h2>
+            <Chatroom />
+          </Route>
         </Switch>
       </Router>
     );
